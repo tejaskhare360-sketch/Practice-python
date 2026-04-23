1c2593be4ab0d59fce13b8e533cb97891f0db990
Tejas Khare <tejaskhare360@gmail.com>
2026-04-23 12:41:06 +0530

    Add files via upload

---
+++ basic chatbot.py
@@ -0,0 +1,26 @@
+print("hi what's up, im your 'predictable' chatbot")
+R1=input()
+if R1 == "hi":
+    print("hello, how can i help you")
+elif  R1 == "why are you predictable":
+    print("coz you created me, and probably know my replies")
+else:
+    print("i dont understand, sir")   
+
+R2=input()
+if R2 == ("calculate my age next year"):
+     print("maybe i ask your name first")
+else: 
+     print("i dont fucking understand")
+R3=input()
+print(f"oh, hello {R3}. tell me your age in 2026, first")
+R4=int(input())
+future=R4+1
+print(f"so next year you will be, {future}")
+R5=input()
+if R5 == "correct":
+     print("great!, now good bye, and let me sleep")
+elif R5 == "wrong":
+     print("then do it by yourself, im sleeping now")
+else:
+     print("fuckyou")
