9fb303ed705608961d12ade3149b7e7b11c9e4e3
Tejas Khare <tejaskhare360@gmail.com>
2026-04-28 22:02:36 +0530

    Add files via upload

---
+++ continue(for loops.py).py
@@ -0,0 +1,18 @@
+for i in range(11):
+     print(i)
+     
+for i in range(2,10):
+     print(i)
+     
+for i in range(2,10,20):
+     print(i)
+     
+     
+#continue the last one (for loops.py)
+
+for i in range(10):
+     print("i love you")
+     
+for i in range(1,11):
+      print(f"got it {i} times")
+   
